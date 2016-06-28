4098bfa61daba47573e0fab0a20907c1f2eb5c42
ming wu <ming wu>
2016-06-28 19:22:19 +0800

    c# 网络层测试

--- test/test_csharp_network/test_csharp_network/test_csharp_network_client/network_client.cs
+++ test/test_csharp_network/test_csharp_network/test_csharp_network_client/network_client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace test_csharp_network_client
 {
@@ -8,6 +9,19 @@ namespace test_csharp_network_client
 	{
 		public network_client()
 		{
+			s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			s.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0));
+			s.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234));
+
+			Console.WriteLine("connected server completed");
+		}
+
+		public static void Main()
+		{
+			var _network_client = new network_client();
+
+			Thread.CurrentThread.Join();
 		}
 
 		private Socket s;
