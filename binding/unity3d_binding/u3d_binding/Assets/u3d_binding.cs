293e94b71af6b9fe27806bc3a2118afbe5c572f2
ming wu <ming wu>
2016-07-21 14:49:22 +0800

    添加c#版client 添加u3d binding

---
+++ binding/unity3d_binding/u3d_binding/Assets/u3d_binding.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+using client;
+
+public class u3d_binding : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        _client = new client.client();
+        _client.connect_server("127.0.0.1", 3236);
+
+        tick = Environment.TickCount;
+        tickcount = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Int64 tmptick = (Environment.TickCount & UInt32.MaxValue);
+        if (tmptick < tick)
+        {
+            tickcount += 1;
+            tmptick = tmptick + tickcount * UInt32.MaxValue;
+        }
+        tick = tmptick;
+
+        _client.poll(tick);
+    }
+
+    private Int64 tickcount;
+    private Int64 tick;
+    private client.client _client;
+
+}
