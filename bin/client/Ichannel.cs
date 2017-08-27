938ddcb54306918c0744fe39d0611635d707620d
qianqians <anxiangqq@gmail.com>
2017-08-27 22:29:17 +0800

    Signed-off-by: qianqians <anxiangqq@gmail.com>

---
+++ bin/client/Ichannel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace juggle
+{
+	public interface Ichannel
+    {
+        void disconnect();
+        ArrayList pop();
+        void push(ArrayList ev);
+    }
+}
