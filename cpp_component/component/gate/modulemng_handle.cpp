16da907276d6d1a3d6b778c50a719a19236cb783
qianqians <451517996@qq.com>
2021-09-29 20:19:34 +0800

    添加全局handle

---
+++ cpp_component/component/gate/modulemng_handle.cpp
@@ -0,0 +1,13 @@
+/*
+ * modulemng_handle.h
+ *
+ *  Created on: 2021-9-29
+ *      Author: qianqians
+ */
+#include "modulemng_handle.h"
+
+namespace gate {
+
+abelkhan::modulemng _modulemng;
+
+}
---
+++ cpp_component/component/gate/modulemng_handle.h
@@ -0,0 +1,18 @@
+/*
+ * modulemng_handle.h
+ *
+ *  Created on: 2021-9-29
+ *      Author: qianqians
+ */
+#ifndef _modulemng_handle_h
+#define _modulemng_handle_h
+
+#include <abelkhan.h>
+
+namespace gate {
+
+extern abelkhan::modulemng _modulemng;
+
+}
+
+#endif //_modulemng_handle_h
