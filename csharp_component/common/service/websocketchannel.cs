6d1921470cbe99b4ccbb4e45e2466b63ba8b031f
吴铭 <451517996@qq.com>
2021-10-05 15:43:27 +0800

    删除废弃代码

--- csharp_component/common/service/websocketchannel.cs
+++ csharp_component/common/service/websocketchannel.cs
@@ -45,30 +45,4 @@ namespace abelkhan
             _socket.Send(data);
         }
     }
-    //public class channel : abelkhan.Ichannel
-    //{
-    //    private IChannelHandlerContext context;
-
-    //    public channel_onrecv _channel_onrecv;
-
-    //    public channel(IChannelHandlerContext _context)
-    //    {
-    //        context = _context;
-    //        _channel_onrecv = new channel_onrecv(this);
-    //    }
-
-    //    public void disconnect()
-    //    {
-    //        context.CloseAsync();
-    //    }
-
-    //    public void send(byte[] data)
-    //    {
-    //        var len = data.Length;
-    //        var initialMessage = Unpooled.Buffer((int)len);
-    //        initialMessage.WriteBytes(data);
-
-    //        context.WriteAndFlushAsync(initialMessage);
-    //    }
-    //}
 }
