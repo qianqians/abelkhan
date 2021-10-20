19e7be0201b8799110a06550ecfe83c169bb2b02
qianqians <451517996@qq.com>
2021-10-20 10:21:30 +0800

    测试cpp hub

--- sample/start_svr.bat
+++ sample/start_svr.bat
@@ -2,4 +2,5 @@ start ./bin/center_svr.exe ./config.txt center &
 start ./bin/dbproxy.exe ./config.txt dbproxy &
 start  ./bin/gate.exe ./config.txt gate &
 start  ./bin/test_csharp.exe ./config.txt test_csharp &
+start  ./bin/test.exe ./config.txt test_cpp &
 pause
