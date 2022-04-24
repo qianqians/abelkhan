1efd0c2c32bf6b592c1826791135a42126058451
qianqians <451517996@qq.com>
2022-04-24 17:33:20 +0800

    完善log模块

--- csharp_component/common/log/log.cs
+++ csharp_component/common/log/log.cs
@@ -16,21 +16,37 @@ namespace log
 
         static public void trace(string log, params object[] agrvs)
         {
+            if (logMode > enLogMode.trace)
+            {
+                return;
+            }
             output(new System.Diagnostics.StackFrame(1), service.timerservice.Tick, "trace", log, agrvs);
         }
 
         static public void debug(string log, params object[] agrvs)
         {
+            if (logMode > enLogMode.debug)
+            {
+                return;
+            }
             output(new System.Diagnostics.StackFrame(1), service.timerservice.Tick, "debug", log, agrvs);
         }
 
         static public void info(string log, params object[] agrvs)
         {
+            if (logMode > enLogMode.info)
+            {
+                return;
+            }
             output(new System.Diagnostics.StackFrame(1), service.timerservice.Tick, "info", log, agrvs);
         }
 
         static public void warn(string log, params object[] agrvs)
         {
+            if (logMode > enLogMode.warn)
+            {
+                return;
+            }
             output(new System.Diagnostics.StackFrame(1), service.timerservice.Tick, "warn", log, agrvs);
         }
 
@@ -41,29 +57,12 @@ namespace log
 
         static void output(StackFrame sf, Int64 tmptime, string level, string log, params object[] agrvs)
         {
-            if (level == "trace" && logMode > enLogMode.trace)
-            {
-                return;
-            }
-            else if (level == "debug" && logMode > enLogMode.debug)
-            {
-                return;
-            }
-            else if (level == "info" && logMode > enLogMode.info)
-            {
-                return;
-            }
-            else if (level == "warn" && logMode > enLogMode.warn)
-            {
-                return;
-            }
-            
             log = string.Format(log, agrvs);
 
             System.DateTime startTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             System.DateTime time = startTime.AddMilliseconds((double)tmptime);
 
-            string strlog = string.Format("[{0}] [{1}] [{2}] [{3}]:{4}", time, level, sf.GetMethod().DeclaringType.FullName, sf.GetMethod().Name, log);
+            string strlog = $"[time] [level] [sf.GetMethod().DeclaringType.FullName] [sf.GetMethod().Name]:log";
 
             lock (logFile)
             {
@@ -74,27 +73,28 @@ namespace log
                     {
                         var tmp = System.IO.File.Create(realLogFile);
                         tmp.Close();
+                        fs = new System.IO.StreamWriter(realLogFile, true);
                     }
                     System.IO.FileInfo finfo = new System.IO.FileInfo(realLogFile);
                     if (finfo.Length > 1024 * 1024 * 32)
                     {
-                        string tmpfile = realLogFile + string.Format(".{0}", time.ToString("yyyy_MM_dd_h_m_s"));
+                        fs.Close();
+                        string tmpfile = string.Format("{0}.{1}", realLogFile, time.ToString("yyyy_MM_dd_h_m_s"));
                         finfo.MoveTo(tmpfile);
                         var tmp = System.IO.File.Create(realLogFile);
                         tmp.Close();
+                        fs = new System.IO.StreamWriter(realLogFile, true);
                     }
                 }
 
                 Console.WriteLine(strlog);
-
-                System.IO.StreamWriter fs = new System.IO.StreamWriter(realLogFile, true);
                 fs.WriteLine(strlog);
-                fs.Close();
             }
         }
 
         static public string logPath = System.Environment.CurrentDirectory;
         static public string logFile = "log.txt";
+        static public System.IO.StreamWriter fs = null;
         static public enLogMode logMode = enLogMode.debug;
     }
 }
