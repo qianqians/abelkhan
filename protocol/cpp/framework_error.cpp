1f8f536134b137c2e8c7940fffc3469a8ff5289b
qianqians <theDarkforce@aliyun.com>
2022-07-03 15:18:27 +0800

    文件重命名error=>framework_error

---
+++ protocol/cpp/framework_error.cpp
@@ -0,0 +1,8 @@
+#include "framework_error.h"
+
+namespace abelkhan
+{
+
+/*this caller code is codegen by abelkhan codegen for cpp*/
+
+}
---
+++ protocol/cpp/framework_error.h
@@ -0,0 +1,25 @@
+#ifndef _h_framework_error_5c19cfdb_e06b_336c_afcf_ec7bc4489acb_
+#define _h_framework_error_5c19cfdb_e06b_336c_afcf_ec7bc4489acb_
+
+#include <abelkhan.h>
+#include <signals.h>
+
+
+namespace abelkhan
+{
+/*this enum code is codegen by abelkhan codegen for cpp*/
+
+    enum class framework_error{
+        enum_framework_timeout = -1,
+        enum_framework_success = 0,
+        enum_framework_client_not_exist = 1,
+        enum_framework_gate_exception = 2
+    };
+
+/*this struct code is codegen by abelkhan codegen for cpp*/
+/*this caller code is codegen by abelkhan codegen for cpp*/
+/*this module code is codegen by abelkhan codegen for cpp*/
+
+}
+
+#endif //_h_framework_error_5c19cfdb_e06b_336c_afcf_ec7bc4489acb_
