831fda5528ca65ddfc1286a68906017c2b618062
qianqians <451517996@qq.com>
2023-04-02 12:00:29 +0800

    python hubproxy

--- py_hub/hubproxy.py
+++ py_hub/hubproxy.py
@@ -1,47 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections;
-using MsgPack.Serialization;
-using System.IO;
-using abelkhan;
+﻿import msgpack
+import abelkhan
+import hub
+from collections.abc import Callable
+import hub_server
 
-namespace hub
-{
-    public class hubproxy
-    {
-        private readonly abelkhan.hub_call_hub_caller _hub_call_hub_caller;
-        private readonly MessagePackSerializer<ArrayList> _serializer = MessagePackSerializer.Get<ArrayList>();
+class hubproxy(object):
+    def __init__(self, hub_name:str, hub_type:str, _ch:abelkhan.Ichannel, _modulemng:abelkhan.modulemng, _hub:hub_server.hub) -> None:
+        self.name = hub_name
+        self.type = hub_type
+        self.ch = _ch
+        self.hub = _hub
+        self.hub_call_hub_caller = hub.hub_call_hub_caller(self.ch, _modulemng)
+    
+    def caller_hub(self, func_name:str, argvs:list):
+        _event = [func_name, argvs]
+        self.hub_call_hub_caller.hub_call_hub_mothed(msgpack.dumps(_event))
 
-        public hubproxy(string hub_name, string hub_type, abelkhan.Ichannel ch)
-        {
-            name = hub_name;
-            type = hub_type;
-            _ch = ch;
-            _hub_call_hub_caller = new abelkhan.hub_call_hub_caller(ch, abelkhan.modulemng_handle._modulemng);
-        }
-
-        public void caller_hub(string func_name, ArrayList argvs)
-        {
-            using var st = MemoryStreamPool.mstMgr.GetStream();
-            var _event = new ArrayList
-            {
-                func_name,
-                argvs
-            };
-
-            _serializer.Pack(st, _event);
-            st.Position = 0;
-
-            _hub_call_hub_caller.hub_call_hub_mothed(st.ToArray());
-        }
-
-        public void client_seep(string client_uuid)
-        {
-            _hub_call_hub_caller.seep_client_gate(client_uuid, hub._gates.get_client_gate_name(client_uuid));
-        }
-
-        public readonly string name;
-        public readonly string type;
-        public readonly abelkhan.Ichannel _ch;
-    }
-}
+    def client_seep(self, client_uuid:str):
+        self.hub_call_hub_caller.seep_client_gate(client_uuid, self.hub._gates.get_client_gate_name(client_uuid))
