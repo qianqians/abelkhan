08092c7f44c0fd7dd492e84b49a3bbe21f745d75
qianqians <451517996@qq.com>
2023-04-02 12:01:26 +0800

    py_hub不与客户端交互

--- py_hub/hubproxy.py
+++ py_hub/hubproxy.py
@@ -5,16 +5,12 @@ from collections.abc import Callable
 import hub_server
 
 class hubproxy(object):
-    def __init__(self, hub_name:str, hub_type:str, _ch:abelkhan.Ichannel, _modulemng:abelkhan.modulemng, _hub:hub_server.hub) -> None:
+    def __init__(self, hub_name:str, hub_type:str, _ch:abelkhan.Ichannel, _modulemng:abelkhan.modulemng) -> None:
         self.name = hub_name
         self.type = hub_type
         self.ch = _ch
-        self.hub = _hub
         self.hub_call_hub_caller = hub.hub_call_hub_caller(self.ch, _modulemng)
     
     def caller_hub(self, func_name:str, argvs:list):
         _event = [func_name, argvs]
         self.hub_call_hub_caller.hub_call_hub_mothed(msgpack.dumps(_event))
-
-    def client_seep(self, client_uuid:str):
-        self.hub_call_hub_caller.seep_client_gate(client_uuid, self.hub._gates.get_client_gate_name(client_uuid))
