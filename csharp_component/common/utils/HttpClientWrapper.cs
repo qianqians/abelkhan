4a5985edc5db6bf488a545bcaad9897817240bb1
qianqians <451517996@qq.com>
2023-05-19 14:39:45 +0800

    HttpClientWrapper

---
+++ csharp_component/common/utils/HttpClientWrapper.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Abelkhan
+{
+    public sealed class HttpClientWrapper
+    {
+        static readonly HttpClient client = new HttpClient();
+        public static void Init()
+        {
+            client.Timeout = TimeSpan.FromSeconds(3);
+        }
+
+        public static async Task<HttpResponseMessage> PostAsync(string url, HttpContent content)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+                return response;
+            }
+            catch (HttpRequestException e)
+            {
+                Log.Log.err("HttpClientWrapper.PostAsync url: {0} error: {1}", url, e);
+                throw;
+            }
+        }
+
+        public static async Task<HttpResponseMessage> GetRspAsync(string url, List<KeyValuePair<string, string>> headers = null)
+        {
+            try
+            {
+                using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+                {
+                    if (headers != null && headers.Count > 0)
+                    {
+                        foreach (var header in headers)
+                        {
+                            request.Headers.Add(header.Key, header.Value);
+                        }
+                    }
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    return response;
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Log.Log.err("HttpClientWrapper.GetRspAsync url: {0} error: {1}", url, e);
+                throw;
+            }
+        }
+    }
+}
