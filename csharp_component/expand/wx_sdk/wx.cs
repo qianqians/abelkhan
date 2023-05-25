bbe7fbba51323c9d38e52a7b0c84144c37f73a96
qianqians <451517996@qq.com>
2023-05-25 17:33:07 +0800

    wxsdk

---
+++ csharp_component/expand/wx_sdk/wx.cs
@@ -0,0 +1,53 @@
+﻿using Abelkhan;
+using Hub;
+using Log;
+using MongoDB.Bson;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace wx
+{
+    public class code2Session
+    {
+        public string openid;
+        public string session_key;
+        public string unionid;
+        public int errcode;
+        public string errmsg;
+    }
+
+    public class wxSdk
+    {
+        public static async Task<code2Session> code2Session(string appid, string secret, string code)
+        {
+            try
+            {
+                var url = $"https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={code}&grant_type=authorization_code";
+                Log.Log.trace("on_player_login:{0}", url);
+                var result = await HttpClientWrapper.GetRspAsync(url);
+
+                if (result != null && result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var ret = await result.Content.ReadAsStringAsync();
+                    Log.Log.trace("jscode2session:{0}", ret);
+                    var ret_obj = Newtonsoft.Json.JsonConvert.DeserializeObject<code2Session>(ret);
+                    return ret_obj;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Log.Log.err($"{ex}");
+                throw;
+            }
+
+            return null;
+        }
+    }
+}
