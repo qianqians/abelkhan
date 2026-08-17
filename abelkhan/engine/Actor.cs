08030e5e9e71f9b0dbd628167390459183c9d0ef
wuming <wuming@lumosplay.com>
2026-08-17 15:32:48 +0800

    Actor

---
+++ abelkhan/engine/Actor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Concurrent;
+
+namespace engine;
+
+interface IJob
+{
+    Task RunAsync();
+}
+
+class SyncJob(Action action) : IJob
+{
+    public Task RunAsync()
+    {
+        action();
+        return Task.CompletedTask;
+    }
+}
+
+class AsyncJob(Func<Task> func) : IJob
+{
+    public Task RunAsync() => func();
+}
+    
+public class Actor
+{
+    private readonly ConcurrentQueue<IJob> _jobs = new();
+
+    public void PostTask(Action action)
+    {
+        _jobs.Enqueue(new SyncJob(action));
+    }
+    
+    public void PostTask(Func<Task> func)
+    {
+        _jobs.Enqueue(new AsyncJob(func));
+    }
+    
+    public async Task Run()
+    {
+        while (_jobs.TryDequeue(out var job))
+        {
+            await job.RunAsync();
+        }
+    }
+}
